297531898fa81698ac7a063f0dd6a32012a205f8
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-15 18:50:05 +0800

    update: changed website layout

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ let mainHeader = document.getElementById("main-header");
 if (randomNumber == 7){
     mainHeader.textContent = "This is not what you are looking for.";
 }
+
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    
+    if (sidebar.classList.contains('active')) {
+        menuToggle.innerHTML = "✕ Close";
+    } else {
+        menuToggle.innerHTML = "☰ Menu";
+    }
+});
