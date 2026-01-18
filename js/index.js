7cb099de8044c6eb3affcfd3f1b067541c92b705
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-18 20:43:35 +0800

    test: layout #10

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ Array.from(navigationLinks).forEach(link => {
     link.addEventListener("click", (event) => {
         const target = event.target.textContent.toLowerCase();
         showContent(target);
-        if (document.getElementsByClassName("mobile-header").style.display != "none"){
+        if (document.getElementsByClassName("mobile-header")[0].style.display != "none"){
             toggleSidebar();
         }
     });
