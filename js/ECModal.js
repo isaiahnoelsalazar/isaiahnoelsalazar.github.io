8f6205edec2e8e00dff94434b921cec64e3df958
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-02-25 12:28:33 +0800

    fix: add user-select styles to close button and modal buttons for better usability

--- js/ECModal.js
+++ js/ECModal.js
@@ -6,7 +6,7 @@ class ECModal {
         <div class="ecmodal-content backgroundColor-white minWidth-300px padding-16px borderRadius-12px position-relative eclistv">
             <div class="ecmodal-header eclisth alignItems-center justifyContent-[space-between] width-100% borderBottom-[solid_#eee_1px] paddingBottom-8px marginBottom-8px">
                 <h3 class="margin-0">${title}</h3>
-                <a class="ecmodal-close close-button ecbounceanimation-5 width-24px height-24px alignItems-center justifyContent-center display-flex borderRadius-8px backgroundColor-#eee hover:backgroundColor-#ddd color-black cursor-pointer">&times;</a>
+                <a class="ecmodal-close close-button ecbounceanimation-5 width-24px height-24px alignItems-center justifyContent-center display-flex borderRadius-8px backgroundColor-#eee hover:backgroundColor-#ddd color-black cursor-pointer" style="user-select: none; -webkit-user-select: none; -ms-user-select: none;">&times;</a>
             </div>
             <div class="ecmodal-body eclisth margin-[8px_0]">
                 <p class="margin-0">${content}</p>
@@ -15,7 +15,7 @@ class ECModal {
             </div>
         </div>`;
         for (let a = 0; a < buttonAmount; a++) {
-            this.modal.querySelector(".ecmodal-footer").insertAdjacentHTML("beforeend", `<a class="ecmodal-button ecbounceanimation-5 padding-[12px_16px] backgroundColor-#1f1f1f hover:backgroundColor-#3f3f3f color-white cursor-pointer">Button ${a+1}</a>`);
+            this.modal.querySelector(".ecmodal-footer").insertAdjacentHTML("beforeend", `<a class="ecmodal-button ecbounceanimation-5 padding-[12px_16px] backgroundColor-#1f1f1f hover:backgroundColor-#3f3f3f color-white cursor-pointer" style="user-select: none; -webkit-user-select: none; -ms-user-select: none;">Button ${a+1}</a>`);
         }
         document.body.appendChild(this.modal);
         this.closeButton = this.modal.querySelector('.close-button');
