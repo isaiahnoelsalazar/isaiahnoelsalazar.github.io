b1f81bd895449f70ffb482ba4d96c4dcc239893a
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-02-25 13:08:15 +0800

    feat: enhance ECModal constructor to support dynamic background and text colors based on dark mode

--- js/ECModal.js
+++ js/ECModal.js
@@ -1,12 +1,12 @@
 class ECModal {
-    constructor({title="Modal", content="Content", buttonAmount=1, buttonLabels=[]} = {}) {
+    constructor({title="Modal", content="Content", buttonAmount=1, buttonLabels=[], backgroundColor="white", color="black", darkMode=false} = {}) {
         this.modal = document.createElement("div");
         this.modal.classList.add("ecmodal", "position-absolute", "height-100%", "width-100%", "display-flex", "backgroundColor-rgba(0,0,0,0.5)", "alignItems-center", "justifyContent-center");
         this.modal.innerHTML = `
-        <div class="ecmodal-content backgroundColor-white minWidth-300px padding-16px borderRadius-12px position-relative eclistv">
+        <div class="ecmodal-content ${darkMode ? "backgroundColor-black color-white" : `backgroundColor-[${backgroundColor}] color-[${color}]`} minWidth-300px padding-16px borderRadius-12px position-relative eclistv">
             <div class="ecmodal-header eclisth alignItems-center justifyContent-[space-between] width-100% borderBottom-[solid_#eee_1px] paddingBottom-8px marginBottom-8px">
                 <h3 class="margin-0">${title}</h3>
-                <a class="ecmodal-close close-button ecbounceanimation-5 width-24px height-24px alignItems-center justifyContent-center display-flex borderRadius-8px backgroundColor-#eee hover:backgroundColor-#ddd color-black cursor-pointer" style="user-select: none; -webkit-user-select: none; -ms-user-select: none;">&times;</a>
+                <a class="ecmodal-close close-button ecbounceanimation-5 width-24px height-24px alignItems-center justifyContent-center display-flex borderRadius-8px ${darkMode ? "backgroundColor-#1f1f1f hover:backgroundColor-#3f3f3f color-white" : "backgroundColor-#eee hover:backgroundColor-#ddd color-black"} cursor-pointer" style="user-select: none; -webkit-user-select: none; -ms-user-select: none;">&times;</a>
             </div>
             <div class="ecmodal-body eclisth margin-[8px_0]">
                 <p class="margin-0">${content}</p>
